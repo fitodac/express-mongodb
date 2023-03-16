7627bec415289e2e35c42a27bac830ed141dc54b
fitodac <fitodac@gmail.com>
2023-03-16 03:12:49 -0300

    CRUD for users

--- network/routes.js
+++ network/routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const message_routes = require('../components/messages/routes')
+const user_routes = require('../components/user/routes')
 
 function routerApi(app){
 	app.use('/message', message_routes)
+	app.use('/user', user_routes)
 }
 
 
